6696f0a425b258f11f5534a75d25ebd40e8dc3a9
Hotrung2311 <trungho2311@gmail.com>
2020-07-29 01:37:40 +0700

    thêm rout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {UserListComponent} from "./users/user-list/user-list.component";
+import {SearchInputComponent} from "./search-input/search-input.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'test-rout', component: UserListComponent},
+  {path: 'abc', component: SearchInputComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
